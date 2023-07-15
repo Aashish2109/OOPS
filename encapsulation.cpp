4c3ec2580b3b3a04f00d10637eae924e78950f11
Aashish Gupta <aashishg863@gmail.com>
2023-07-15 23:55:09 +0530

    Encapsulation in OOPS

---
+++ encapsulation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Main
+{
+private:
+    int age;
+    int weight;
+
+public:
+    int getage()
+    {
+        return age;
+    }
+};
+int main()
+{
+    Main object;
+    cout<<object.getage();
+}
